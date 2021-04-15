222c40f7156ca469fd8a0abe87243e0ca8729151
unknown <kml.hareza@gmail.com>
2021-04-15 22:01:02 +0200

    ADD prettier and eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,35 @@
-import firebase from "../src/config/firebase.config";
+import React, { FC } from 'react';
+import { GetStaticProps } from 'next';
+import firebase from '../src/config/firebase.config';
+import { IEvent } from '../src/types/event';
 
-export interface IHome {
-  events: { id: string; title: string; date: string }[];
-}
-
-const Home = ({ events }: IHome) => {
-  return (
-    <div>
-      {events.map((event) => (
-        <div key={event.id}>{event.title}</div>
-      ))}
-    </div>
-  );
+export type Props = {
+  events: IEvent[];
 };
 
-export const getStaticProps = async () => {
-  const res = await firebase.firestore().collection("events").get();
+const Home: FC<Props> = ({ events }) => (
+  <div>
+    {events.map((event) => (
+      <div key={event.id}>{event.title}</div>
+    ))}
+  </div>
+);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await firebase.firestore().collection('events').get();
 
   const events = res.docs
     .map((doc) => ({ id: doc.id, data: doc.data() }))
     .map(({ id, data }) => ({
       id,
       ...data,
-      date: new Date(data.date.seconds * 1000).toLocaleString(),
+      date: new Date(data.date.seconds * 1000).toLocaleString()
     }));
 
   return {
     props: {
-      events,
-    },
+      events
+    }
   };
 };
 
--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,9 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
-import "firebase/analytics";
-import "firebase/performance";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/analytics';
+import 'firebase/performance';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,15 +12,15 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
   // Check that `window` is in scope for the analytics module!
-  if (typeof window !== "undefined") {
+  if (typeof window !== 'undefined') {
     // Enable analytics. https://firebase.google.com/docs/analytics/get-started
-    if ("measurementId" in firebaseConfig) {
+    if ('measurementId' in firebaseConfig) {
       firebase.analytics();
       firebase.performance();
     }
